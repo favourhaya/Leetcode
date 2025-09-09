64a74371d399b68bd95450301089f6c0fad82509
favourhaya <favouryahaya102@gmail.com>
2025-09-10 00:17:47 +0100

    solved

---
+++ 98.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        high = float("inf")
+        low = float("-inf")
+        def check(root,low,high):
+            if not root:
+                return True
+             
+            if  root.val >= high:
+                return False
+            if root.val <= low:
+                return False
+            
+            return check(root.left,low,root.val) and check(root.right,root.val,high)
+        return check(root,low,high)
