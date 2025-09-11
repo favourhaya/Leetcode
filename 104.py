6def8b2ae08104f89c21e3579dbac2b0b0b44074
favourhaya <favouryahaya102@gmail.com>
2025-09-11 22:59:34 +0100

    solved again

--- 104.py
+++ 104.py
@@ -25,3 +25,22 @@ class Solution:
                     queue.append(curr.right)
         return level
         
+
+
+#dfs solution:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        left = 1 + self.maxDepth(root.left)
+        right = 1+ self.maxDepth(root.right)
+
+        return max(left,right)
+
+            
